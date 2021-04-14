e924ed186c704215559ac7d0a8d634da7e78eab2
superfeeling-git <58456025+superfeeling-git@users.noreply.github.com>
2021-04-14 23:29:16 +0800

    实现无限级分类的Cascader显示及图片上传

--- shop/src/page/Goods/Create.vue
+++ shop/src/page/Goods/Create.vue
@@ -3,8 +3,12 @@
         <el-col :span="14">
             <el-form :model="form" :rules="rules" ref="form" label-width="180px">
                 <el-form-item label="商品分类">
-                    <el-select v-model="form.categoryId" placeholder="请选择所属分类">
-                    </el-select>
+                    <el-cascader
+                        v-model="form.value"
+                        :options="form.options"
+                        placeholder="做为一级分类"
+                        @change="handleChange">
+                    </el-cascader>
                 </el-form-item>
                 <el-form-item label="商品名称" prop="goodsName">
                     <el-input v-model="form.goodsName" placeholder="请输入内容"></el-input>
@@ -14,6 +18,18 @@
                 </el-form-item>
                 <el-form-item label="添加时间">
                     <el-date-picker type="date" placeholder="选择日期" v-model="form.addTime" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="图片上传">
+                  <el-upload
+                    class="avatar-uploader"
+                    :action="form.actions"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    name="formFile"
+                    :before-upload="beforeAvatarUpload">
+                    <img v-if="form.imageUrl" :src="form.imageUrl" class="avatar">
+                    <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+                  </el-upload>
                 </el-form-item>
 
                 <el-form-item>
@@ -26,10 +42,40 @@
 </template>
 
 <style scoped>
-.el-select{display: block;}
+.el-cascader{display: block;}
+</style>
+
+<!--上传文件样式-->
+<style>
+  .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .el-upload:hover {
+    border-color: #409EFF!important;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
 
 <script>
+import baseUrl from '../../util/evns'
+import {GetAllCategory} from '../../util/apis'
+
   export default {
     data() {
       return {
@@ -38,7 +84,11 @@
           goodsId: '',
           goodsName: '',
           goodsPrice: 1,
-          addTime: ''
+          addTime: '',
+          value:[],
+          options: [],
+          imageUrl:"",
+          actions:`${baseUrl.baseUrl}Goods/UploadFile`
         },
         rules:{
             goodsName: [
@@ -58,7 +108,29 @@
             return false;
           }
         });
+      },
+      handleChange(value) {
+        console.log(value);
+      },
+      handleAvatarSuccess(res, file, fileList) {
+        this.form.imageUrl = URL.createObjectURL(file.raw);
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        return isJPG && isLt2M;
       }
-    }
+    },mounted() {
+      GetAllCategory().then(d=>{
+        this.form.options = d.data;
+      });
+    },
   }
 </script>
--- shop/src/util/apis.js
+++ shop/src/util/apis.js
@@ -3,6 +3,9 @@ import request from '../util/request'
 //登录
 export let Login = (data) => request("Account/Login" , data , "Post");
 
+//获取所有商品分类
+export let GetAllCategory = () => request("Category/Index");
+
 //商品添加
 export let GoodsCreate = (data) => request("Goods/Create" , data , "Post");
 
